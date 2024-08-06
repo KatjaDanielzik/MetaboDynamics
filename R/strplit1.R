abcc501c0f4a9c0bb08981f8e13be7781b3d2465
KatjaDanielzik <katja.danielzik@uni-due.de>
2024-08-06 22:31:22 +0200

    "readme"

--- R/strplit1.R
+++ R/strplit1.R
@@ -1,3 +1,12 @@
+#' Title
+#'
+#' @param x a delimiter
+#' @param split a string
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
