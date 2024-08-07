987dca0494997cad0bcbdce7dc044f5f52f3c531
KatjaDanielzik <katja.danielzik@uni-due.de>
2024-08-07 12:25:31 +0200

    "data/intra including documentation draft"

--- data-raw/data_sim.R
+++
@@ -1,8 +0,0 @@
-# here should be the code stored to convert raw data into data that is
-# available for the user
-
-# this is only dummy code!!!! to enable practising of data documentation
-process <- function(a,b,c){
-  temp <- a+b+c
-  return(temp)
-}
