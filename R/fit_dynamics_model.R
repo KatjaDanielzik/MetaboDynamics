95913129c8a5ee234e907495e15554a7cdb9b21f
KatjaDanielzik <katja.danielzik@uni-due.de>
2024-08-07 22:36:48 +0200

    fit_dynamics_model function without test + documentation draft

--- R/fit_dynamics_model.R
+++ R/fit_dynamics_model.R
@@ -1,11 +1,19 @@
-#' Title
+
+#' Fits dynamic model
 #'
-#' @param data
-#' @param metabolite
-#' @param condition
+#' MetaboDynamics provides a hierachical model for robust estimation of mean
+#' concentrations over time of single metabolites at single experimental conditions.
 #'
-#' @return
+#' @param data concentration table containing the columns "metabolite", "condition", "cpc_stand" for default
+#' @param metabolite column of "data" that contains the metabolite names or IDs
+#' @param time column of "time" that contains time as numeric, make sure your time column is ordered from lowest to highest for the model to work
+#' @param condition column of "data" that contains the experimental conditions
+#' @param cpc column of "data" that contains the concentrations per cell, centered and normalized per metabolite and experimental condition (mean=0, sd=1)
+#'
+#' @return returns one modelfit named fit_condition per experimental condition
 #' @export
+#'
+#' @examples No examples yet
 #'
 #' @import methods
 #' @import Rcpp
@@ -13,31 +21,38 @@
 #' @importFrom rstantools rstan_config
 #' @importFrom RcppParallel RcppParallelLibs
 #' @useDynLib MetaboDynamics
-#'
-#' @examples
-fit_dynamics_model <- function(data,metabolite,condition){
-rstan::sampling(stanmodels$m_ANOVA_partial_pooling)
+
+fit_dynamics_model <- function(data=intra,metabolite="metabolite",time="time",condition="dose",cpc="log_cpc_stand"){
+# get unique experimental conditions
+conditions <- unique(data[[condition]])
+# loop over conditions
+for(i in 1:length(conditions)){
+  # subset dataframe
+temp <- data[data[[condition]]==conditions[i],]
+
+# fit model
+fit <- rstan::sampling(object=stanmodels$m_ANOVA_partial_pooling,
+                       data=list(y=temp[[cpc]],
+                                 t=length(unique(temp[[time]])),
+                                 M=length(unique(temp[[metabolite]])),
+                                 N=nrow(temp)),
+                                 # Vector of metabolite IDs
+                                 Me=as.numeric(as.factor(temp[[metabolite]])),
+                                 # Vector indicating which row belongs to which timestep
+                                 X=as.numeric(as.factor(as.numeric(temp[[time]]))),
+                       chains = 4,
+                       iter = 4000,
+                      # increase warmup so that algorithm probably chooses
+                      #smaller step sizes for sampling the posterior
+                       warmup = 1000,
+                       algorithm = "NUTS",
+                       cores = 4,
+                       # increase adapt_delta (target average proposal acceptance probability)
+                       # to decrease step size
+                       control=list(adapt_delta=.999,max_treedepth=14))
+# assign condition name to fit and return
+return(assign(paste0("fit","_",conditions[i]),fit))
+}
 }
 
-# N <- nrow(temp)
-# t <- length(unique(temp$time))
-# M <- length(unique(temp$metabolite))
-# r <- 3 # number of replicates
-# fit_complete_pooling <- rstan::sampling(object = compiled_model_complete_pooling,
-#                                         data = list(y = temp$log_cpc_stand,
-#                                                     t = length(unique(temp$time)),
-#                                                     M = length(unique(temp$metabolite)),
-#                                                     N = nrow(temp),
-#                                                     Me= as.numeric(as.factor(temp$metabolite)), # 98 metabolites * 4 time points
-#                                                     # Vector indicating which row belongs to which timestep
-#                                                     X=as.numeric(as.factor(as.numeric(temp$time)))),
-#                                         chains = 4,
-#                                         iter = 4000,
-#                                         # increase warmup so that algorithm probably chooses
-#                                         # smaller step sizes for sampling the posterior
-#                                         warmup = 1000,
-#                                         algorithm = "NUTS",
-#                                         cores = 4,
-#                                         # increase adapt_delta (target average proposal acceptance probability)
-#                                         # to decrease step size
-#                                         control=list(adapt_delta=.999,max_treedepth=14))
+
---
+++ tests/testthat/test-fit_dynamics_model.R
@@ -0,0 +1,3 @@
+test_that("model fit gets returned", {
+  exists("fit_0Gy")
+})
